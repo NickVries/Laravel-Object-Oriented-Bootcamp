36469ff1d25661066a8702dd2e7233099c0c3c57
Nick de Vries <nickdevries93@gmail.com>
2017-10-19 11:05:44 +0200

    :sprakles: Added Person class

---
+++ Person.php
@@ -0,0 +1,31 @@
+<?php
+
+class Person
+{
+    public $name;
+
+    public $age;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setAge($age)
+    {
+        if ($age < 18) {
+            throw new Exception('Person is not old enough.');
+        }
+        $this->age = $age;
+    }
+
+    public function getAge()
+    {
+        return $this->age * 365;
+    }
+}
+
+$John = new Person('John Doe');
+$John->setAge(30);
+
+var_dump($John->getAge());
